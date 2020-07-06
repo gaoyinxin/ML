799abb2538ec1b23de1b5be7f50c3831ee3e14a8
Yinxin Gao <yinxin.gao@rakuten.com>
2020-07-06 13:36:44 +0900

    vgg16 test

--- README.md
+++ README.md
@@ -9,16 +9,22 @@ pip install --upgrade Keras==2.3.1
 
 ## Test Data
 
-### MNIST
+### MNIST (11M)
 https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz
 ```shell script
 mv mnist.npz ~/.keras/datasets
 ```
 
-### Cifar 
+### Cifar (170M)
 https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 ```shell script
 mv cifar-10-python.tar.gz ~/.keras/datasets
 cd ~/.keras/datasets
 mv cifar-10-python.tar.gz cifar-10-batches-py.tar.gz
+```
+
+### VGG16 Model (500M)
+https://storage.googleapis.com/tensorflow/keras-applications/vgg16/vgg16_weights_tf_dim_ordering_tf_kernels.h5
+```shell script
+mv vgg16_weights_tf_dim_ordering_tf_kernels.h5 ~/.keras/models
 ```
---
+++ src/5_DeepCNN/vgg16_test.py
@@ -0,0 +1,31 @@
+import sys
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.applications.vgg16 import VGG16
+
+'''
+What's VGG: https://zhuanlan.zhihu.com/p/41423739
+'''
+def func():
+    # prebuild model with pre-trained weights on imagenet
+    model = VGG16(weights='imagenet', include_top=True)
+    model.compile(optimizer='sgd', loss='categorical_crossentropy')
+
+    # resize into VGG16 trained images' format
+    im = cv2.resize(cv2.imread('../../resource/img/dog.jpeg'), (224, 224)).astype(np.float32)
+    im = np.expand_dims(im, axis=0)
+
+    # https://gist.github.com/yrevar/942d3a0ac09ec9e5eb3a
+    out = model.predict(im)
+    index = np.argmax(out)
+    print(index)
+    plt.plot(out.ravel())
+    plt.show()
+    # this should print 820 for steaming train
+
+
+if __name__ == "__main__":
+    func()
+    sys.exit()
